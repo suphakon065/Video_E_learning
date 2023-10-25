3e10cd78afa389a56ad566160fa933e3e22394c7
suphakon065 <139618514+suphakon065@users.noreply.github.com>
2023-10-26 00:31:13 +0700

    Update EditAccountComponent.js

--- react/src/Component/EditAccountComponent.js
+++ react/src/Component/EditAccountComponent.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import Typography from '@mui/material/Typography'
 import { Box } from "@mui/system";
-import { Avatar, Button, FormControl, Grid, Icon, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Avatar, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Grid, Icon, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import HeaderBar from "./HeaderBarComponent";
@@ -16,8 +16,10 @@ import axios from "axios";
 import { indigo } from "@mui/material/colors";
 import config from "../Storage/config";
 import Servicesconfig from "../Storage/servicesConfig";
-import { EditUser, EditUserImg } from "../services/api";
+import { EditPassword, EditUser, EditUserImg } from "../services/api";
 import { EditAccountAvatarStyles, EditAccountBoxStyles, EditAccountButtonStyles, EditAccountFormControlPasswordStyles, EditAccountGridAllContentStyles, EditAccountGridButtonStyles, EditAccountGridHeadStyles, EditAccountGridIDStyles, EditAccountGridMidSpaceStyles, EditAccountGridNameStyles, EditAccountGridNewPasswordStyles, EditAccountGridPasswordStyles, EditAccountGridsubtltleStyles, EditAccountInputAdornmentStyles, EditAccountInputInputStyles, EditAccountSaveIconStyles, EditAccountTypographyBlankStyles, EditAccountTypographySubtitleStyles, EditAccountTypographyTitleStyles } from "./Styles/EditAccountPageStyles";
+import { CardSubjectTeacherBoldTextStyles, CardSubjectTeacherCancleStyles, CardSubjectTeacherGridDeleteSubjectStyles, CardSubjectTeacherYesStyles } from "./Styles/CardSubjectTeacherPageStyles";
+import { loginErrorTextGridStyles, loginErrorTypographyStyles } from "./Styles/LoginPageStyles";
 
 const EditAccount = () =>{
     const location = useLocation();
@@ -33,6 +35,9 @@ const EditAccount = () =>{
     });
     const [fileImg,setFileImg] = useState();
     const [Img,setImg] = useState();
+    const [dialogStatus,setDialogStatus] = useState(false);
+    const [dialogChangePasswordStatus,setDialogChangePasswordStatus] = useState(false);
+    const [textError,setTextError] = useState(false);
     const navigate = useNavigate();
     const AccontPagePath = config.AccountPage;
 
@@ -65,26 +70,31 @@ const EditAccount = () =>{
             return Servicesconfig.getImageProfile+index.User_PIC
         }
     }
-
+    const handleChangePassword = async()=>{
+        const changePasswordService = await EditPassword(UserData.Uid,userForm);
+        if(changePasswordService){
+            setTextError(false);
+            setDialogChangePasswordStatus(false);
+            navigateToContacts();
+        }else{
+            setTextError(true);
+        }
+    }
     const handleSave = async()=>{
-            try{
-                let picName = '';
-                if(fileImg!==undefined){
-                    let rand = Math.floor(Math.random()* 10000);
-                    const picLastName = userForm.stdId+ fileImg[0].name.substr(fileImg[0].name.length - 4);
-                    picName = rand.toString()+'-'+picLastName.toLowerCase();
-                }else{
-                    picName=userForm.User_PIC
-                }
-
-                const response = await EditUser(UserData.Uid,userForm,picName);
-                console.log('Response: ',response);
-                console.log('userId ',UserData.Uid);
-                console.log('userForm ',userForm);
-                console.log('ImageName ', picName);
-                localStorage.setItem('Name',userForm.stdId);
+        try{
+            let picName = '';
+            if(fileImg!==undefined){
+                let rand = Math.floor(Math.random()* 10000);
+                const picLastName = userForm.stdId+ fileImg[0].name.substr(fileImg[0].name.length - 4);
+                picName = rand.toString()+'-'+picLastName.toLowerCase();
+            }else{
+                picName=userForm.User_PIC
+            }
+            const response = await EditUser(UserData.Uid,userForm,picName);
+            if(response === true){
+                setTextError(false);
+                setDialogStatus(false);
                 navigateToContacts();
-                
                 if(fileImg!==undefined){
                     try{
                         const responseImgEdit = await EditUserImg(fileImg,picName);
@@ -96,13 +106,18 @@ const EditAccount = () =>{
                 }else{
                     navigateToContacts();
                 }
-            }catch(error){
-                console.error(error);
+            }else{
+                setTextError(true);
             }
-            
-            
+            console.log('Response: ',response);
+            console.log('userId ',UserData.Uid);
+            console.log('userForm ',userForm);
+            console.log('ImageName ', picName);
+            // localStorage.setItem('Name',userForm.stdId);
+        }catch(error){
+            console.error('error',error);
+        }  
     }
-
     return(
         <div>
             <Box sx={EditAccountBoxStyles}>
@@ -143,12 +158,12 @@ const EditAccount = () =>{
                     
                     <Grid container sx={EditAccountGridIDStyles}>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
-                            {/* <Typography variant="caption" sx={EditAccountTypographyBlankStyles}>&nbsp;</Typography> */}
+                            
                             <Typography 
                                 variant="subtitle1" 
                                 sx={EditAccountTypographySubtitleStyles}
                             >
-                                student ID
+                                ID
                             </Typography>
                         </Grid>
                         <Grid item xs={12} sm={8} md={8} lg={8} xl={8} sx={EditAccountGridMidSpaceStyles}>
@@ -162,7 +177,6 @@ const EditAccount = () =>{
                                 placeholder='Student ID' 
                                 type={'text'} 
                                 sx={EditAccountInputInputStyles}
-                                onChange={handleChange}
                                 size={"small"}
                             />
                         </Grid>
@@ -188,9 +202,8 @@ const EditAccount = () =>{
                             />
                         </Grid>
                     </Grid>
-                    <Grid container sx={EditAccountGridPasswordStyles}>
+                    {/* <Grid container sx={EditAccountGridPasswordStyles}>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
-                            {/* <Typography variant="caption" textAlign="left">&nbsp;</Typography> */}
                             <Typography variant="subtitle1" sx={EditAccountTypographySubtitleStyles}>
                                 Password
                             </Typography>
@@ -219,10 +232,9 @@ const EditAccount = () =>{
                                 />
                             </FormControl>
                         </Grid>
-                    </Grid>
-                    <Grid container sx={EditAccountGridNewPasswordStyles}>
+                    </Grid> */}
+                    {/* <Grid container sx={EditAccountGridNewPasswordStyles}>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
-                            {/* <Typography variant="caption" textAlign="left">&nbsp;</Typography> */}
                             <Typography variant="subtitle2" sx={EditAccountTypographySubtitleStyles}>
                                 New Password
                             </Typography>
@@ -251,23 +263,195 @@ const EditAccount = () =>{
                                 />
                             </FormControl>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
                     <Grid container sx={EditAccountGridButtonStyles}>
+                        <Grid item>
+                            <Button
+                                sx={{marginTop:2,color:indigo[900]}}
+                                variant="text"
+                                onClick={() =>{setDialogChangePasswordStatus(true)}}
+                            >
+                                Change Password
+                            </Button>
+                        </Grid>
                         <Grid item>
                             <Button fullWidth
                                 sx={EditAccountButtonStyles} 
                                 variant="contained"
-                                onClick={handleSave}
+                                onClick={()=>{setDialogStatus(true)}}
+                                startIcon={<SaveIcon/>}
                             >
-                                <Icon>
-                                    <SaveIcon sx={EditAccountSaveIconStyles}/>
-                                </Icon>
-                                &nbsp;Save 
+                                Save 
                             </Button>
                         </Grid>
                     </Grid>
                 </Grid>
             </Box>
+            <Dialog
+                open={dialogStatus}
+                onClose={() =>{}}
+                scroll="paper"
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    <Typography variant="body1"
+                        sx={CardSubjectTeacherBoldTextStyles}
+                    >
+                        กรุณากรอกรหัสผ่านเพื่อยืนยัน
+                    </Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container>
+                        <FormControl sx={EditAccountFormControlPasswordStyles} variant="outlined">
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                name='stdPassword'
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            edge="end"
+                                        >
+                                            {!showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                sx={EditAccountInputAdornmentStyles}
+                                value = {userForm.stdPassword}
+                                onChange={handleChange}
+                                size={"small"}
+                            />
+                        </FormControl>
+                    </Grid>
+                    {textError?
+                        <Grid container 
+                            sx={loginErrorTextGridStyles}
+                        >
+                            <Grid item>
+                                <Typography sx={loginErrorTypographyStyles} >รหัสผ่านไม่ถูกต้อง</Typography>
+                            </Grid>
+                        </Grid>:null
+                    }
+                </DialogContent>
+                <DialogActions>
+                <Button
+                    onClick={()=>{setDialogStatus(false)}}
+                    autoFocus
+                    style={CardSubjectTeacherCancleStyles}
+                >
+                    No
+                </Button>
+                <Button 
+                    style={CardSubjectTeacherYesStyles}
+                    onClick={handleSave}
+                >
+                    Yes
+                </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog
+                open={dialogChangePasswordStatus}
+                onClose={() =>{setDialogChangePasswordStatus(false)}}
+                scroll="paper"
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    <Typography variant="body1"
+                        sx={CardSubjectTeacherBoldTextStyles}
+                    >
+                        เปลี่ยนรหัสผ่าน
+                    </Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container>
+                        <Typography sx={EditAccountTypographySubtitleStyles}>
+                            Password
+                        </Typography>
+                    </Grid>
+                    <Grid container>
+                        <FormControl sx={EditAccountFormControlPasswordStyles} variant="outlined">
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                name='stdPassword'
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            edge="end"
+                                        >
+                                            {!showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                sx={EditAccountInputAdornmentStyles}
+                                value = {userForm.stdPassword}
+                                onChange={handleChange}
+                                size={"small"}
+                            />
+                        </FormControl>
+                    </Grid>
+                    <Grid container>
+                        <Typography sx={EditAccountTypographySubtitleStyles}>
+                            New password
+                        </Typography>
+                    </Grid>
+                    <Grid container>
+                        <FormControl sx={EditAccountFormControlPasswordStyles} variant="outlined">
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                name='stdNewPassword'
+                                type={shownewPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShownewPassword}
+                                            edge="end"
+                                        >
+                                            {!shownewPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                sx={EditAccountInputAdornmentStyles}
+                                value = {userForm.stdNewPassword}
+                                onChange={handleChange}
+                                size={"small"}
+                            />
+                        </FormControl>
+                    </Grid>
+                    {textError?
+                        <Grid container 
+                            sx={loginErrorTextGridStyles}
+                        >
+                            <Grid item>
+                                <Typography sx={loginErrorTypographyStyles} >รหัสผ่านไม่ถูกต้อง</Typography>
+                            </Grid>
+                        </Grid>:null
+                    }
+                </DialogContent>
+                <DialogActions>
+                <Button
+                    onClick={()=>{setDialogChangePasswordStatus(false)}}
+                    autoFocus
+                    style={CardSubjectTeacherCancleStyles}
+                >
+                    No
+                </Button>
+                <Button 
+                    style={CardSubjectTeacherYesStyles}
+                    onClick={handleChangePassword}
+                >
+                    Yes
+                </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
